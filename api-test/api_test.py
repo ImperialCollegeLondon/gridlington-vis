3b64c04cd00d596189461acbe4e3ae5cef83fff3
CWestICL <cwest@ic.ac.uk>
2023-02-01 15:15:21 +0000

    init config

--- api-test/api_test.py
+++ api-test/api_test.py
@@ -1,5 +1,5 @@
-import pip._vendor.requests
-import ast
+import requests
+import json
 
 
 APP_URL = "http://146.179.34.13:8080/app/html"
@@ -10,125 +10,69 @@ PLOT_URL = "http://liionsden.rcs.ic.ac.uk:8050"
 # Initial config for sections
 INIT_SECTIONS = [
     {
-        "x":500,
-        "y":0,
-        "w":1920,
-        "h":1080,
-        "space":"PC01-Top",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot1"
-                }
-            }
-        }
+        "x": 500,
+        "y": 0,
+        "w": 1920,
+        "h": 1080,
+        "space": "PC01-Top",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot1"}}},
     },
     {
-        "x":0,
-        "y":0,
-        "w":1920,
-        "h":1080,
-        "space":"PC01-Left",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot2"
-                }
-            }
-        }
+        "x": 0,
+        "y": 0,
+        "w": 1920,
+        "h": 1080,
+        "space": "PC01-Left",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot2"}}},
     },
     {
-        "x":0,
-        "y":0,
-        "w":1920,
-        "h":1080,
-        "space":"PC01-Right",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot3"
-                }
-            }
-        }
+        "x": 0,
+        "y": 0,
+        "w": 1920,
+        "h": 1080,
+        "space": "PC01-Right",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot3"}}},
     },
     {
-        "x":0,
-        "y":0,
-        "w":1920,
-        "h":1080,
-        "space":"PC02-Top",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot4"
-                }
-            }
-        }
+        "x": 0,
+        "y": 0,
+        "w": 1920,
+        "h": 1080,
+        "space": "PC02-Top",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot4"}}},
     },
     {
-        "x":0,
-        "y":0,
-        "w":1920,
-        "h":1080,
-        "space":"PC02-Left",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot5"
-                }
-            }
-        }
+        "x": 0,
+        "y": 0,
+        "w": 1920,
+        "h": 1080,
+        "space": "PC02-Left",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot5"}}},
     },
     {
-        "x":0,
-        "y":0,
-        "w":1920,
-        "h":1080,
-        "space":"PC02-Right",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot6"
-                }
-            }
-        }
+        "x": 0,
+        "y": 0,
+        "w": 1920,
+        "h": 1080,
+        "space": "PC02-Right",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot6"}}},
     },
     {
-        "x":0,
-        "y":0,
-        "w":3840,
-        "h":2160,
-        "space":"Hub01",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot7"
-                }
-            }
-        }
+        "x": 0,
+        "y": 0,
+        "w": 3840,
+        "h": 2160,
+        "space": "Hub01",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot7"}}},
     },
     {
-        "x":0,
-        "y":0,
-        "w":3840,
-        "h":2160,
-        "space":"Hub02",
-        "app":{
-            "url":APP_URL,
-            "states": {
-                "load": {
-                    "url": f"{PLOT_URL}/plot8"
-                }
-            }
-        }
-    }
+        "x": 0,
+        "y": 0,
+        "w": 3840,
+        "h": 2160,
+        "space": "Hub02",
+        "app": {"url": APP_URL, "states": {"load": {"url": f"{PLOT_URL}/plot8"}}},
+    },
 ]
 
 section_one = {
@@ -156,45 +100,52 @@ section_two = {
 }
 
 template = {
-        "space": "SpaceOne",
-        "x": 0,
-        "y": 0,
-        "w": 1440,
-        "h": 808,
-        "app": {
-            "url": APP_URL,
-            "states": {"load": {"url": f"{PLOT_URL}/plot1"}},
-        },
-    }
+    "space": "SpaceOne",
+    "x": 0,
+    "y": 0,
+    "w": 1440,
+    "h": 808,
+    "app": {
+        "url": APP_URL,
+        "states": {"load": {"url": f"{PLOT_URL}/plot1"}},
+    },
+}
 
 
 # Function for creating all initial sections
 def create_all():
     for section in INIT_SECTIONS:
-        response = pip._vendor.requests.post("http://146.179.34.13:8080/section", json=section)
+        response = requests.post("http://146.179.34.13:8080/section", json=section)
+        print(response.text)
+
+
+# Function for creating all initial sections
+def create_all():
+    for section in INIT_SECTIONS:
+        response = requests.post("http://146.179.34.13:8080/section", json=section)
         print(response.text)
 
 
 # Function for sending request to create section one in specified space arg
 def create_section_one(space):
     section_one["space"] = space
-    response = pip._vendor.requests.post(f"{API_URL}/section", json=section_one)
+    response = requests.post(f"{API_URL}/section", json=section_one)
     print(response.text)
 
 
 # Function for sending request to create section two in specified space arg
 def create_section_two(space):
     section_two["space"] = space
-    response = pip._vendor.requests.post(f"{API_URL}/section", json=section_two)
+    response = requests.post(f"{API_URL}/section", json=section_two)
     print(response.text)
 
 
 # Function to move a section by id to a specified space
 def move_section(id_num, space):
     url = f"{API_URL}/sections/{id_num}?includeAppStates=true"
-    response = pip._vendor.requests.get(url)
-    #print(response.text)
-    data = ast.literal_eval(response.text)
+    response = requests.get(url)
+    # print(response.text)
+    data = json.loads(response.text)
 
     template["space"] = space
     template["x"] = data["x"]
@@ -204,19 +155,19 @@ def move_section(id_num, space):
     template["app"] = data["app"]
 
     url = f"{API_URL}/sections/{id_num}"
-    response = pip._vendor.requests.post(url, json=template)
+    response = requests.post(url, json=template)
     print(response.text)
 
 
 # Function to swap the spaces of two sections by id
 def swap_sections(id_a, id_b):
     url = f"{API_URL}/sections/{id_a}?includeAppStates=true"
-    response = pip._vendor.requests.get(url)
-    data_a = ast.literal_eval(response.text)
+    response = requests.get(url)
+    data_a = json.loads(response.text)
 
     url = f"{API_URL}/sections/{id_b}?includeAppStates=true"
-    response = pip._vendor.requests.get(url)
-    data_b = ast.literal_eval(response.text)
+    response = requests.get(url)
+    data_b = json.loads(response.text)
 
     template["space"] = data_b["space"]
     template["y"] = data_a["y"]
@@ -226,7 +177,7 @@ def swap_sections(id_a, id_b):
     template["app"] = data_a["app"]
 
     url = f"{API_URL}/sections/{id_a}"
-    response = pip._vendor.requests.post(url, json=template)
+    response = requests.post(url, json=template)
     print(response.text)
 
     template["space"] = data_a["space"]
@@ -237,22 +188,22 @@ def swap_sections(id_a, id_b):
     template["app"] = data_b["app"]
 
     url = f"{API_URL}/sections/{id_b}"
-    response = pip._vendor.requests.post(url, json=template)
+    response = requests.post(url, json=template)
     print(response.text)
 
 
 # Function for deleting all sections
 def delete_all():
-    response = pip._vendor.requests.get(f"{API_URL}/sections")
-    data = ast.literal_eval(response.text)
+    response = requests.get(f"{API_URL}/sections")
+    data = json.loads(response.text)
     id_nums = []
 
     for section in data:
         id_nums.append(section["id"])
-    
+
     for num in id_nums:
         url = f"{API_URL}/sections/{num}"
-        pip._vendor.requests.delete(url)
+        requests.delete(url)
 
 
 delete_all()
