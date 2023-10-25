010d8596dbe49a9639c639c406bef11964f257d6
Travis Gordon <tg2414@ic.ac.uk>
2023-10-25 12:05:38 +0100

    Added in some of the solution from Gridlington-datahub #98.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
 """The main module for the Vis system."""
+
+import logging
+import logging.config
+
+from .core.log_config import logging_dict_config
+
+logging.config.dictConfig(logging_dict_config)
+
+log = logging.getLogger("api_logger")
+log.debug("Logging is configured.")
+log.info("Datahub API is running...")
