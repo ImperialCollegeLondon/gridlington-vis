272a3ce14664521360236dd0f21976995e4a262d
pclemow <philip.clemow@imperial.ac.uk>
2024-08-05 15:17:20 +0100

    Added Maps Page - Now with formatting

--- app/pages/map.py
+++ app/pages/map.py
@@ -1,4 +1,5 @@
 """Map view page in dash app.
+
 This is a cut down copy of the agent page
 One plots:
 - Agent and EV Locations
@@ -6,17 +7,12 @@ One plots:
 
 import dash  # type: ignore
 import pandas as pd
-import plotly.express as px  # type: ignore
 import plotly.graph_objects as go  # type: ignore
 from dash import Input, Output, callback, dcc  # type: ignore
 
 from .. import log
 from ..figures import (
-    generate_agent_activity_breakdown_fig,
-    generate_dsr_commands_fig,
-    generate_ev_charging_breakdown_fig,
     generate_map_fig,
-    generate_sld_fig,
 )
 from ..layout import GridBuilder
 
@@ -63,6 +59,4 @@ def update_figures(
     # TODO: ensure each figure is using the correct dataframe
     map_fig = generate_map_fig(DF_OPAL)
     log.debug("Updating figures on Map page")
-    return (
-        map_fig,
-    )
+    return (map_fig,)
